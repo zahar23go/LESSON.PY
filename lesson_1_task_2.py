890772bad7b99d8559a6a7a3dafa3a7cb6fe646f
zahar23go <zahar-c23@mail.ru>
2026-01-02 15:15:12 +0300

    Выполнение домашнего задания по уроку 1: задачи 1-5

--- lesson_1_task_2.py
+++ lesson_1_task_2.py
@@ -1,3 +1,13 @@
 my_age =  39
 my_age = my_age+3
 print(my_age)
+
+
+
+
+my_age = 39  
+
+my_age = 39+3  
+
+
+print(my_age)
--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -13,3 +13,17 @@ print_number(3)
 print_number(5)
 print_number(3)
 print_number(5)
+
+
+
+
+
+
+
+def print_number(num):
+    print(num, end='')
+
+# Номер 88005553535
+number = [8, 8, 0, 0, 5, 5, 5, 3, 5, 3, 5]
+for digit in number:
+    print_number(digit)
